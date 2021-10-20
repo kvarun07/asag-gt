b30a2f8efae55a12635482a7eab8abaf95075334
Varun Khurana <varun19124@iiitd.ac.in>
2021-10-21 02:37:08 +0530

    Initial Commit

---
+++ data/old_molecules_1.py
@@ -0,0 +1,416 @@
+import torch
+import pickle
+import torch.utils.data
+import time
+import os
+import numpy as np
+
+import csv
+
+import dgl
+
+from scipy import sparse as sp
+import numpy as np
+import networkx as nx
+import hashlib
+import random
+import math
+
+
+class MoleculeDGL(torch.utils.data.Dataset):
+    def __init__(self, data_dir, split, num_graphs=None):
+        self.data_dir = data_dir
+        self.split = split
+        self.num_graphs = num_graphs
+        
+        with open(data_dir + "/%s.pickle" % self.split,"rb") as f:
+            self.data = pickle.load(f)
+
+        if self.num_graphs in [10000, 1000]:
+            # loading the sampled indices from file ./zinc_molecules/<split>.index
+            with open(data_dir + "/%s.index" % self.split,"r") as f:
+                data_idx = [list(map(int, idx)) for idx in csv.reader(f)]
+                self.data = [ self.data[i] for i in data_idx[0] ]
+
+            assert len(self.data)==num_graphs, "Sample num_graphs again; available idx: train/val/test => 10k/1k/1k"
+        
+        """
+        data is a list of Molecule dict objects with following attributes
+        
+          molecule = data[idx]
+        ; molecule['num_atom'] : nb of atoms, an integer (N)
+        ; molecule['atom_type'] : tensor of size N, each element is an atom type, an integer between 0 and num_atom_type
+        ; molecule['bond_type'] : tensor of size N x N, each element is a bond type, an integer between 0 and num_bond_type
+        ; molecule['logP_SA_cycle_normalized'] : the chemical property to regress, a float variable
+        """
+        
+        self.graph_lists = []
+        self.graph_labels = []
+        self.n_samples = len(self.data)
+        self._prepare()
+    
+    def _prepare(self):
+        print("preparing %d graphs for the %s set..." % (self.num_graphs, self.split.upper()))
+        count = 0
+        for molecule in self.data:
+            node_features = molecule['atom_type'].long()
+            
+            adj = molecule['bond_type']
+            edge_list = (adj != 0).nonzero()  # converting adj matrix to edge_list
+            
+            edge_idxs_in_adj = edge_list.split(1, dim=1)
+            edge_features = adj[edge_idxs_in_adj].reshape(-1).long()
+            
+            # Create the DGL Graph
+            g = dgl.DGLGraph()
+            g.add_nodes(molecule['num_atom'])
+            g.ndata['feat'] = node_features
+            
+            for src, dst in edge_list:
+                g.add_edges(src.item(), dst.item())
+            g.edata['feat'] = edge_features
+            
+            self.graph_lists.append(g)
+            self.graph_labels.append(molecule['logP_SA_cycle_normalized'])
+
+            if count == 0: 
+                print(g)
+                print(g.nodes)
+                print(g.edges)
+                print(g.ndata)
+                print(g.edata)
+                count += 1
+
+        
+    def __len__(self):
+        """Return the number of graphs in the dataset."""
+        return self.n_samples
+
+    def __getitem__(self, idx):
+        """
+            Get the idx^th sample.
+            Parameters
+            ---------
+            idx : int
+                The sample index.
+            Returns
+            -------
+            (dgl.DGLGraph, int)
+                DGLGraph with node feature stored in `feat` field
+                And its label.
+        """
+        return self.graph_lists[idx], self.graph_labels[idx]
+    
+    
+class MoleculeDatasetDGL(torch.utils.data.Dataset):
+    def __init__(self, name='Zinc'):
+        t0 = time.time()
+        self.name = name
+        
+        self.num_atom_type = 28 # known meta-info about the zinc dataset; can be calculated as well
+        self.num_bond_type = 4 # known meta-info about the zinc dataset; can be calculated as well
+        
+        data_dir='./data/molecules'
+        
+        if self.name == 'ZINC-full':
+            data_dir='./data/molecules/zinc_full'
+            self.train = MoleculeDGL(data_dir, 'train', num_graphs=220011)
+            self.val = MoleculeDGL(data_dir, 'val', num_graphs=24445)
+            self.test = MoleculeDGL(data_dir, 'test', num_graphs=5000)
+        else:            
+            self.train = MoleculeDGL(data_dir, 'train', num_graphs=10000)
+            self.val = MoleculeDGL(data_dir, 'val', num_graphs=1000)
+            self.test = MoleculeDGL(data_dir, 'test', num_graphs=1000)
+        print("Time taken: {:.4f}s".format(time.time()-t0))
+        
+
+
+def self_loop(g):
+    """
+        Utility function only, to be used only when necessary as per user self_loop flag
+        : Overwriting the function dgl.transform.add_self_loop() to not miss ndata['feat'] and edata['feat']
+        
+        
+        This function is called inside a function in MoleculeDataset class.
+    """
+    new_g = dgl.DGLGraph()
+    new_g.add_nodes(g.number_of_nodes())
+    new_g.ndata['feat'] = g.ndata['feat']
+    
+    src, dst = g.all_edges(order="eid")
+    src = dgl.backend.zerocopy_to_numpy(src)
+    dst = dgl.backend.zerocopy_to_numpy(dst)
+    non_self_edges_idx = src != dst
+    nodes = np.arange(g.number_of_nodes())
+    new_g.add_edges(src[non_self_edges_idx], dst[non_self_edges_idx])
+    new_g.add_edges(nodes, nodes)
+    
+    # This new edata is not used since this function gets called only for GCN, GAT
+    # However, we need this for the generic requirement of ndata and edata
+    new_g.edata['feat'] = torch.zeros(new_g.number_of_edges())
+    return new_g
+
+
+def make_full_graph(g):
+    """
+        Converting the given graph to fully connected
+        This function just makes full connections
+        removes available edge features 
+    """
+
+    full_g = dgl.from_networkx(nx.complete_graph(g.number_of_nodes()))
+    full_g.ndata['feat'] = g.ndata['feat']
+    full_g.edata['feat'] = torch.zeros(full_g.number_of_edges()).long()
+    
+    try:
+        full_g.ndata['lap_pos_enc'] = g.ndata['lap_pos_enc']
+    except:
+        pass
+
+    try:
+        full_g.ndata['wl_pos_enc'] = g.ndata['wl_pos_enc']
+    except:
+        pass    
+    
+    return full_g
+
+
+
+def laplacian_positional_encoding(g, pos_enc_dim):
+    """
+        Graph positional encoding v/ Laplacian eigenvectors
+    """
+
+    # Laplacian
+    A = g.adjacency_matrix_scipy(return_edge_ids=False).astype(float)
+    N = sp.diags(dgl.backend.asnumpy(g.in_degrees()).clip(1) ** -0.5, dtype=float)
+    L = sp.eye(g.number_of_nodes()) - N * A * N
+
+    # Eigenvectors with numpy
+    EigVal, EigVec = np.linalg.eig(L.toarray())
+    idx = EigVal.argsort() # increasing order
+    EigVal, EigVec = EigVal[idx], np.real(EigVec[:,idx])
+    g.ndata['lap_pos_enc'] = torch.from_numpy(EigVec[:,1:pos_enc_dim+1]).float() 
+    
+    print("LapPE = ", g.ndata['lap_pos_enc'].size())
+    print("L.size = ", L.shape)
+    print("idx = ", idx)
+    return g
+
+def wl_positional_encoding(g):
+    """
+        WL-based absolute positional embedding 
+        adapted from 
+        
+        "Graph-Bert: Only Attention is Needed for Learning Graph Representations"
+        Zhang, Jiawei and Zhang, Haopeng and Xia, Congying and Sun, Li, 2020
+        https://github.com/jwzhanggy/Graph-Bert
+    """
+    max_iter = 2
+    node_color_dict = {}
+    node_neighbor_dict = {}
+
+    edge_list = torch.nonzero(g.adj().to_dense() != 0, as_tuple=False).numpy()
+    node_list = g.nodes().numpy()
+
+    # setting init
+    for node in node_list:
+        node_color_dict[node] = 1
+        node_neighbor_dict[node] = {}
+
+    for pair in edge_list:
+        u1, u2 = pair
+        if u1 not in node_neighbor_dict:
+            node_neighbor_dict[u1] = {}
+        if u2 not in node_neighbor_dict:
+            node_neighbor_dict[u2] = {}
+        node_neighbor_dict[u1][u2] = 1
+        node_neighbor_dict[u2][u1] = 1
+
+
+    # WL recursion
+    iteration_count = 1
+    exit_flag = False
+    while not exit_flag:
+        new_color_dict = {}
+        for node in node_list:
+            neighbors = node_neighbor_dict[node]
+            neighbor_color_list = [node_color_dict[neb] for neb in neighbors]
+            color_string_list = [str(node_color_dict[node])] + sorted([str(color) for color in neighbor_color_list])
+            color_string = "_".join(color_string_list)
+            hash_object = hashlib.md5(color_string.encode())
+            hashing = hash_object.hexdigest()
+            new_color_dict[node] = hashing
+        color_index_dict = {k: v+1 for v, k in enumerate(sorted(set(new_color_dict.values())))}
+        for node in new_color_dict:
+            new_color_dict[node] = color_index_dict[new_color_dict[node]]
+        if node_color_dict == new_color_dict or iteration_count == max_iter:
+            exit_flag = True
+        else:
+            node_color_dict = new_color_dict
+        iteration_count += 1
+        
+    g.ndata['wl_pos_enc'] = torch.LongTensor(list(node_color_dict.values()))
+    return g
+
+class DataUnit(torch.utils.data.Dataset):
+    def __init__(self, raw_list):
+        self.size = len(raw_list)
+        self.graph_lists = [ i[0] for i in raw_list ]
+        self.graph_labels = [ i[1] for i in raw_list ]
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        return self.graph_lists[idx], self.graph_labels[idx]
+
+class MoleculeDataset(torch.utils.data.Dataset):
+
+    # def __init__(self, name):
+    #     """
+    #         Loading ZINC dataset
+    #     """
+    #     start = time.time()
+    #     print("[I] Loading dataset %s..." % (name))
+    #     self.name = name
+    #     data_dir = 'data/molecules/'
+    #     with open(data_dir+name+'.pkl',"rb") as f:
+    #         f = pickle.load(f)
+    #         self.train = f[0]
+    #         self.val = f[1]
+    #         self.test = f[2]
+    #         self.num_atom_type = f[3]
+    #         self.num_bond_type = f[4]
+    #     print('train, test, val sizes :',len(self.train),len(self.test),len(self.val))
+    #     print("[I] Finished loading.")
+    #     print("[I] Data load time: {:.4f}s".format(time.time()-start))
+
+    def __init__(self, name):
+        """
+            Loading ZINC dataset
+        """
+        start = time.time()
+        print("[I] Loading dataset %s..." % (name))
+        self.name = name
+        data_dir = 'data/molecules/'
+        # with open(data_dir+name+'.pkl',"rb") as f:
+        with open('data/graph_score.pickle',"rb") as f:
+            f = pickle.load(f)
+            # self.train = f[0]
+            # self.val = f[1]
+            # self.test = f[2]
+            # self.num_atom_type = f[3]
+            # self.num_bond_type = f[4]
+
+            print("\nORIGINAL GRAPHS")
+            print(f)
+            print(f"Data size = {len(f)} graphs!")
+            # print(f[0])
+            # print(f[0][0])
+            # print(f[0][0].nodes(data=True))
+            # print(f[0][0].nodes.data)
+            # print(f[0][0].edges())
+            # print(f[0][0].nodes.data.items())
+
+            print(f[1])
+            print(f[1][0])
+            print(f[1][0].nodes(data=True))
+            print(f[1][0].nodes.data)
+            print(f[1][0].edges())
+            # print(f[1][0].nodes.data.items())
+
+            print("\nCONVERTED GRAPHS")
+            old_list = [(dgl.from_networkx(i[0]), torch.tensor([i[1]])) for i in f]
+            g_list = []
+
+            for i in old_list:
+              grh = i[0]
+              grh.ndata['feat'] = torch.tensor(len(grh.nodes()) * [1])
+              # grh.ndata['feat'] = torch.ones(len(grh.nodes()), 1)
+              grh.edata['feat'] = torch.ones(grh.number_of_edges(), 1)
+              g_list.append((grh, i[1]))
+
+            print(g_list[1])
+            print(g_list[1][0].nodes())
+            print(g_list[1][0].edges())
+            print(f"Data size = {len(g_list)} graphs!")
+
+            # Define train/test/val splits in the ratio of 8:1:1
+            # First shuffle g_list
+            random.shuffle(g_list)
+            # print(g_list[0])
+            total = len(g_list)
+            part1 = math.ceil(0.8*total)
+            part2 = math.ceil(0.9*total)
+            self.train = DataUnit(g_list[:part1])
+            self.val = DataUnit(g_list[part1:part2])
+            self.test = DataUnit(g_list[part2:])
+
+            # print(len(self.train), len(self.val), len(self.test))
+            print(self.train.size, self.val.size, self.test.size)
+
+            # self.train.graph_lists = [ i[0] for i in self.train ]
+            # self.val.graph_lists = [ i[0] for i in self.val ]
+            # self.test.graph_lists = [ i[0] for i in self.test ]
+
+            # self.train.graph_labels = [ i[1] for i in self.train ]
+            # self.val.graph_lists = [ i[1] for i in self.val ]
+            # self.test.graph_lists = [ i[1] for i in self.test ]
+
+            print("Self.TRAIN = ", self.train)
+            print(self.train.__getitem__(2))
+            print(self.train.__len__())
+            print(self.train.__getitem__(2)[0].nodes())
+            print(self.train.__getitem__(2)[0].edges())
+            print("N_DATA =", self.train.__getitem__(2)[0].ndata['feat'].size(), "\n", self.train.__getitem__(2)[0].ndata['feat'])
+            print(type(self.train))
+
+        # print('train, test, val sizes :',len(self.train),len(self.test),len(self.val))
+        print("[I] Finished loading.")
+        print("[I] Data load time: {:.4f}s".format(time.time()-start))
+
+
+    # form a mini batch from a given list of samples = [(graph, label) pairs]
+    def collate(self, samples):
+        # The input samples is a list of pairs (graph, label).
+        graphs, labels = map(list, zip(*samples))
+        labels = torch.tensor(np.array(labels)).unsqueeze(1)
+        batched_graph = dgl.batch(graphs)       
+        
+        return batched_graph, labels
+    
+    
+    def _add_self_loops(self):
+        
+        # function for adding self loops
+        # this function will be called only if self_loop flag is True
+            
+        self.train.graph_lists = [self_loop(g) for g in self.train.graph_lists]
+        self.val.graph_lists = [self_loop(g) for g in self.val.graph_lists]
+        self.test.graph_lists = [self_loop(g) for g in self.test.graph_lists]
+
+    def _make_full_graph(self):
+        
+        # function for converting graphs to full graphs
+        # this function will be called only if full_graph flag is True
+        self.train.graph_lists = [make_full_graph(g) for g in self.train.graph_lists]
+        self.val.graph_lists = [make_full_graph(g) for g in self.val.graph_lists]
+        self.test.graph_lists = [make_full_graph(g) for g in self.test.graph_lists]
+    
+    
+    def _add_laplacian_positional_encodings(self, pos_enc_dim):
+        
+        # Graph positional encoding v/ Laplacian eigenvectors
+        self.train.graph_lists = [laplacian_positional_encoding(g, pos_enc_dim) for g in self.train.graph_lists]
+        self.val.graph_lists = [laplacian_positional_encoding(g, pos_enc_dim) for g in self.val.graph_lists]
+        self.test.graph_lists = [laplacian_positional_encoding(g, pos_enc_dim) for g in self.test.graph_lists]
+
+    def _add_wl_positional_encodings(self):
+        
+        # WL positional encoding from Graph-Bert, Zhang et al 2020.
+        self.train.graph_lists = [wl_positional_encoding(g) for g in self.train.graph_lists]
+        self.val.graph_lists = [wl_positional_encoding(g) for g in self.val.graph_lists]
+        self.test.graph_lists = [wl_positional_encoding(g) for g in self.test.graph_lists]
+
+
---
+++ nets/molecules_graph_regression/.ipynb_checkpoints/graph_transformer_net-checkpoint.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import dgl
+
+"""
+    Graph Transformer with edge features
+    
+"""
+from layers.graph_transformer_edge_layer import GraphTransformerLayer
+from layers.mlp_readout_layer import MLPReadout
+
+class GraphTransformerNet(nn.Module):
+    def __init__(self, net_params):
+        super().__init__()
+        # num_atom_type = net_params['num_atom_type']
+        # num_bond_type = net_params['num_bond_type']
+
+        # in_dim_node = net_params['in_dim']
+
+        hidden_dim = net_params['hidden_dim']
+        num_heads = net_params['n_heads']
+        out_dim = net_params['out_dim']
+        in_feat_dropout = net_params['in_feat_dropout']
+        dropout = net_params['dropout']
+        n_layers = net_params['L']
+        self.readout = net_params['readout']
+        self.layer_norm = net_params['layer_norm']
+        self.batch_norm = net_params['batch_norm']
+        self.residual = net_params['residual']
+        self.edge_feat = net_params['edge_feat']
+        self.device = net_params['device']
+        self.lap_pos_enc = net_params['lap_pos_enc']
+        self.wl_pos_enc = net_params['wl_pos_enc']
+        max_wl_role_index = 37 # this is maximum graph size in the dataset
+        
+        if self.lap_pos_enc:
+            pos_enc_dim = net_params['pos_enc_dim']
+            self.embedding_lap_pos_enc = nn.Linear(pos_enc_dim, hidden_dim)
+        if self.wl_pos_enc:
+            self.embedding_wl_pos_enc = nn.Embedding(max_wl_role_index, hidden_dim)
+
+        # What should replace num_atom_type here?
+        # self.embedding_h = nn.Embedding(num_atom_type, hidden_dim)
+        # torch.unique(dataset.train[0][0].ndata['feat'],dim=0).size(0)
+        # self.embedding_h = nn.Embedding(10000, 256)
+        # self.embedding_h = nn.Embedding(100, hidden_dim)
+        # self.embedding_h = nn.Embedding(in_dim_node, hidden_dim)
+        # print("arch embedding_h = ", self.embedding_h)
+
+        if self.edge_feat:
+            self.embedding_e = nn.Embedding(num_bond_type, hidden_dim)
+        else:
+            self.embedding_e = nn.Linear(1, hidden_dim)
+        
+        self.in_feat_dropout = nn.Dropout(in_feat_dropout)
+        
+        self.layers = nn.ModuleList([ GraphTransformerLayer(hidden_dim, hidden_dim, num_heads, dropout,
+                                                    self.layer_norm, self.batch_norm, self.residual) for _ in range(n_layers-1) ]) 
+        self.layers.append(GraphTransformerLayer(hidden_dim, out_dim, num_heads, dropout, self.layer_norm, self.batch_norm, self.residual))
+        # self.MLP_layer = MLPReadout(out_dim, 1)   # 1 out dim since regression problem        
+        
+    def forward(self, g, h, e, h_lap_pos_enc=None, h_wl_pos_enc=None):
+
+        # input embedding
+        # h = torch.tensor(h).to(0).long() # device: 0 is cuda gpu
+        # h = h.clone().detach().long()
+        # h = h.to(torch.long)
+
+        # print()
+        # print("(1) forward embedding_h, h = ", self.embedding_h, h.size())
+        # print(h)
+        # h = self.embedding_h(h)
+        # print("(2) forward embedding_h, h = ", self.embedding_h, h.size())
+        # print(h)
+        # print()
+        
+        h = self.in_feat_dropout(h)
+        if self.lap_pos_enc:
+            h_lap_pos_enc = self.embedding_lap_pos_enc(h_lap_pos_enc.float()) 
+            h = h + h_lap_pos_enc
+        if self.wl_pos_enc:
+            h_wl_pos_enc = self.embedding_wl_pos_enc(h_wl_pos_enc) 
+            h = h + h_wl_pos_enc
+        if not self.edge_feat: # edge feature set to 1
+            e = torch.ones(e.size(0),1).to(self.device)
+        e = self.embedding_e(e)   
+        
+        # print(h.shape, e.shape)
+        
+        # convnets
+        for conv in self.layers:
+            h, e = conv(g, h, e)
+        g.ndata['h'] = h
+        
+        if self.readout == "sum":
+            hg = dgl.sum_nodes(g, 'h')
+        elif self.readout == "max":
+            hg = dgl.max_nodes(g, 'h')
+        elif self.readout == "mean":
+            hg = dgl.mean_nodes(g, 'h')
+        else:
+            hg = dgl.mean_nodes(g, 'h')  # default readout is mean nodes
+            
+        # return self.MLP_layer(hg)
+        return hg
+        
+    # def loss(self, scores, targets):
+    #     # loss = nn.MSELoss()(scores,targets)
+    #     loss = nn.L1Loss()(scores, targets)
+    #     return loss
