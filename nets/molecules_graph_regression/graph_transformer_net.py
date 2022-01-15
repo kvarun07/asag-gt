5f855fa4e03003ec1656bcc734addf135c3f0ac6
Varun Khurana <varun19124@iiitd.ac.in>
2022-01-16 03:24:22 +0530

    clean gt_net.py

--- nets/molecules_graph_regression/graph_transformer_net.py
+++ nets/molecules_graph_regression/graph_transformer_net.py
@@ -9,16 +9,11 @@ import dgl
     
 """
 from layers.graph_transformer_edge_layer import GraphTransformerLayer
-# from layers.graph_transformer_layer import GraphTransformerLayer
 from layers.mlp_readout_layer import MLPReadout
 
 class GraphTransformerNet(nn.Module):
     def __init__(self, net_params):
         super().__init__()
-        # num_atom_type = net_params['num_atom_type']
-        # num_bond_type = net_params['num_bond_type']
-
-        # in_dim_node = net_params['in_dim']
 
         hidden_dim = net_params['hidden_dim']
         num_heads = net_params['n_heads']
@@ -42,14 +37,6 @@ class GraphTransformerNet(nn.Module):
         if self.wl_pos_enc:
             self.embedding_wl_pos_enc = nn.Embedding(max_wl_role_index, hidden_dim)
 
-        # What should replace num_atom_type here?
-        # self.embedding_h = nn.Embedding(num_atom_type, hidden_dim)
-        # torch.unique(dataset.train[0][0].ndata['feat'],dim=0).size(0)
-        # self.embedding_h = nn.Embedding(10000, 256)
-        # self.embedding_h = nn.Embedding(100, hidden_dim)
-        # self.embedding_h = nn.Embedding(in_dim_node, hidden_dim)
-        # print("arch embedding_h = ", self.embedding_h)
-
         if self.edge_feat:
             self.embedding_e = nn.Embedding(num_bond_type, hidden_dim)
         else:
@@ -63,19 +50,6 @@ class GraphTransformerNet(nn.Module):
         # self.MLP_layer = MLPReadout(out_dim, 1)   # 1 out dim since regression problem        
         
     def forward(self, g, h, e, h_lap_pos_enc=None, h_wl_pos_enc=None):
-
-        # input embedding
-        # h = torch.tensor(h).to(0).long() # device: 0 is cuda gpu
-        # h = h.clone().detach().long()
-        # h = h.to(torch.long)
-
-        # print()
-        # print("(1) forward embedding_h, h = ", self.embedding_h, h.size())
-        # print(h)
-        # h = self.embedding_h(h)
-        # print("(2) forward embedding_h, h = ", self.embedding_h, h.size())
-        # print(h)
-        # print()
         
         h = self.in_feat_dropout(h)
         if self.lap_pos_enc:
@@ -86,14 +60,11 @@ class GraphTransformerNet(nn.Module):
             h = h + h_wl_pos_enc
         if not self.edge_feat: # edge feature set to 1
             e = torch.ones(e.size(0),1).to(self.device)
-        e = self.embedding_e(e)   
-        
-        # print(h.shape, e.shape)
+        e = self.embedding_e(e)
         
         # convnets
         for conv in self.layers:
             h, e = conv(g, h, e)
-            # h = conv(g, h)
         g.ndata['h'] = h
         
         if self.readout == "sum":
@@ -104,11 +75,6 @@ class GraphTransformerNet(nn.Module):
             hg = dgl.mean_nodes(g, 'h')
         else:
             hg = dgl.mean_nodes(g, 'h')  # default readout is mean nodes
-            
-        # return self.MLP_layer(hg)
+
         return hg
-        
-    # def loss(self, scores, targets):
-    #     # loss = nn.MSELoss()(scores,targets)
-    #     loss = nn.L1Loss()(scores, targets)
-    #     return loss
+
